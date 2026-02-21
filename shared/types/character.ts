68269a16de2f89af4befba9032df089647f90fa5
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2026-02-21 19:21:00 +0100

    feat: format code

--- shared/types/character.ts
+++ shared/types/character.ts
@@ -1,33 +1,33 @@
-type CharacterArchetype = 'king' | 'queen' | 'jack';
-export type CharacterSuit = 'hearts' | 'clubs' | 'spades'; // diamonds exluded. In "The house doesn't always win", diamonds are the bad guys, so they are not a valid suit for the characters.
+type CharacterArchetype = "king" | "queen" | "jack";
+export type CharacterSuit = "hearts" | "clubs" | "spades"; // diamonds exluded. In "The house doesn't always win", diamonds are the bad guys, so they are not a valid suit for the characters.
 type StatModifier = -2 | -1 | 0 | 1 | 2;
 
 interface CharacterItem {
-    name: string
-    concealed: boolean
+  name: string;
+  concealed: boolean;
 }
 interface CharacterIdentity {
-    name: string
-    pronouns?: string
-    concept?: string
-    weapon?: CharacterItem
-    instrument?: CharacterItem
+  name: string;
+  pronouns?: string;
+  concept?: string; // Who is this character? A brief description of their personality, background, or role in the story.
+  weapon?: CharacterItem;
+  instrument?: CharacterItem;
 }
 
 interface CharacterSkill {
-    name: string
-    description: string
-    uses?: {
-        usesLeft: number
-        maxUses: number
-    }
+  name: string;
+  description: string;
+  uses?: {
+    usesLeft: number;
+    maxUses: number;
+  };
 }
 export interface CharacterSheet {
-    archetype: CharacterArchetype
-    suit: CharacterSuit
-    damage: { [suit in CharacterSuit]: boolean }
-    modifiers: { [stat in keyof CharacterSheet]: StatModifier }
-    suitSkill: CharacterSkill // TODO: create enum based on the suit-archetype combinations
-    characterIdentity: CharacterIdentity
-    skills: CharacterSkill[] // TODO: create enum based on the archetype skills
+  archetype: CharacterArchetype;
+  suit: CharacterSuit;
+  damage: { [suit in CharacterSuit]: boolean };
+  modifiers: { [stat in keyof CharacterSheet]: StatModifier };
+  suitSkill: CharacterSkill; // TODO: create enum based on the suit-archetype combinations
+  characterIdentity: CharacterIdentity;
+  skills: CharacterSkill[]; // TODO: create enum based on the archetype skills
 }
