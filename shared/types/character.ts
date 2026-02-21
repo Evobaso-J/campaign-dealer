57069b1f61f07e7fe234a4054c65b04084456334
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2026-02-21 17:15:53 +0100

    feat: add character type

---
+++ shared/types/character.ts
@@ -0,0 +1,34 @@
+type CharacterArchetype = 'king' | 'queen' | 'jack';
+type CharacterSuit = 'hearts' | 'clubs' | 'spades'; // diamonds exluded. In "The house doesn't always win", diamonds are the bad guys, so they are not a valid suit for the characters.
+type StatModifier = -2 | -1 | 0 | 1 | 2;
+
+interface CharacterItem {
+    name: string
+    concealed: boolean
+}
+interface CharacterIdentity {
+    name: string
+    pronouns?: string
+    concept?: string
+    weapon?: CharacterItem
+    instrument?: CharacterItem
+}
+
+interface CharacterSkill {
+    name: string
+    description: string
+    uses?: {
+        usesLeft: number
+        maxUses: number
+    }
+}
+
+export interface CharacterSheet {
+    archetype: CharacterArchetype
+    suit: CharacterSuit
+    damage: { [suit in CharacterSuit]: boolean }
+    modifiers: { [stat in keyof CharacterSheet]: StatModifier }
+    skill: string // TODO: create enum based on the suit-archetype combinations
+    characterIdentity: CharacterIdentity
+    skills: CharacterSkill[] // TODO: create enum based on the archetype skills
+}
