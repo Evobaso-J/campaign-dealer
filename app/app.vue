9a439cd2c7042424d302b4585064188263f76d82
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2026-03-04 22:43:11 +0100

    feat: add crt flicker

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
   <UApp>
+    <div class="crt-overlay" aria-hidden="true" />
+    <div class="crt-vignette" aria-hidden="true" />
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </UApp>
