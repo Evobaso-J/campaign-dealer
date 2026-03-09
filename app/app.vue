8a479fe10722469419c4ff096f075b663717aa3c
Andrea Basile <57828270+Evobaso-J@users.noreply.github.com>
2026-03-09 16:20:33 +0100

    feat: add base layout (#29)

--- app/app.vue
+++ app/app.vue
@@ -2,10 +2,10 @@
   <UApp>
     <div class="crt-overlay" aria-hidden="true" />
     <div class="crt-vignette" aria-hidden="true" />
-    <header class="fixed top-0 right-0 z-50 p-3">
-      <ThemePicker />
-    </header>
-    <NuxtRouteAnnouncer />
-    <NuxtPage />
+
+    <NuxtLayout>
+      <NuxtRouteAnnouncer />
+      <NuxtPage />
+    </NuxtLayout>
   </UApp>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-8 max-w-4xl mx-auto pixel-border-thick">
+  <div class="p-8 max-w-4xl mx-auto">
     <WizardStepper />
   </div>
 </template>
