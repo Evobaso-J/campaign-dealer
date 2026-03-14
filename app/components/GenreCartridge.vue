b86f54d7e10a0635964cfbfc37f25eb0e2d7c80b
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2026-03-14 19:18:58 +0100

    feat: update genre cartridge

--- app/components/GenreCartridge.vue
+++ app/components/GenreCartridge.vue
@@ -12,10 +12,10 @@ defineEmits<{
 </script>
 
 <template>
-  <div class="aspect-square" :class="genreGroupConfig[group].class">
+  <div class="aspect-square relative" :class="genreGroupConfig[group].class">
     <UIcon
       :name="genreIcons[genre]"
-      class="text-2xl z-10"
+      class="text-8xl z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
       :class="genreGroupConfig[group].icon"
     />
   </div>
